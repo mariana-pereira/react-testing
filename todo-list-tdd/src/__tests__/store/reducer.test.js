ef22902cc9fa8ede17d22859874400a76ec6652b
Mariana Pereira <marianna.22sp@gmail.com>
2020-09-18 15:59:22 -0300

    todo-list-tdd: coverage report

--- todo-list-tdd/src/__tests__/store/reducer.test.js
+++ todo-list-tdd/src/__tests__/store/reducer.test.js
@@ -2,6 +2,12 @@ import reducer, { INITIAL_STATE } from '../../store/modules/tasks/reducer';
 import * as Tasks from '../../store/modules/tasks/actions';
 
 describe('Tasks reducer', () => {
+  it('DEFAULT', () => {
+    const state = reducer(undefined, {});
+
+    expect(state).toStrictEqual(INITIAL_STATE);
+  });
+
   it('ADD_TASK', () => {
     const state = reducer(INITIAL_STATE, Tasks.addTask('make coffee'));
 
