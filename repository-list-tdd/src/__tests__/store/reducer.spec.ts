44b995a876d77c7c6f71b0aafbdadae79a6f02c5
Mariana Pereira <marianna.22sp@gmail.com>
2020-09-29 12:59:32 -0300

    repository-list-tdd: coverage report

--- repository-list-tdd/src/__tests__/store/reducer.spec.ts
+++ repository-list-tdd/src/__tests__/store/reducer.spec.ts
@@ -2,6 +2,12 @@ import reducer, { INITIAL_STATE } from '../../store/modules/repositories/reducer
 import * as Repositories from '../../store/modules/repositories/actions';
 
 describe('Repositories reducer', () => {
+  it('DEFAULT', () => {
+    const state = reducer(undefined, {});
+
+    expect(state).toStrictEqual(INITIAL_STATE);
+  });
+
   it('ADD_NEW_REPOSITORY', () => {
     const state = reducer(INITIAL_STATE, Repositories.addNewRepository('react-testing'));
 
