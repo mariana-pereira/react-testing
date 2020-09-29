8d4bcc3352447168e7e5ec1abd005e028d20ca0d
Mariana Pereira <marianna.22sp@gmail.com>
2020-09-29 11:29:47 -0300

    repository-list-tdd: mock axios

--- repository-list-tdd/src/__tests__/store/sagas.spec.ts
+++ repository-list-tdd/src/__tests__/store/sagas.spec.ts
@@ -1,13 +1,30 @@
 import { runSaga } from 'redux-saga';
-import { getRepositoriesSuccess } from '../../store/modules/repositories/actions';
+import MockAdapter from 'axios-mock-adapter';
+import api from '../../services/api';
+
+import { getRepositoriesSuccess, getRepositoriesFailure } from '../../store/modules/repositories/actions';
 import { getRepositories } from '../../store/modules/repositories/sagas';
+
+const apiMock = new MockAdapter(api);
 
 describe('Repositories saga', () => {
   it('should be able to fetch repositories', async () => {
     const dispatch = jest.fn();
 
+    apiMock.onGet('repositories').reply(200, ['react-testing']);
+
     await runSaga({ dispatch }, getRepositories).toPromise();
 
     expect(dispatch).toHaveBeenCalledWith(getRepositoriesSuccess(['react-testing']));
+  });
+
+  it('should fail when api returns error', async () => {
+    const dispatch = jest.fn();
+
+    apiMock.onGet('repositories').reply(500);
+
+    await runSaga({ dispatch }, getRepositories).toPromise();
+
+    expect(dispatch).toHaveBeenCalledWith(getRepositoriesFailure());
   });
 });
